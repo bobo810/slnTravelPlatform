5d62fb50a32f6b713b5609febf18b1a0a20004a0
bobo <jeffrey880810@gmail.com>
2024-02-13 17:22:56 +0800

    加入專案檔案。

---
+++ prjTravelPlatformV3/Areas/Employee/Controllers/Employee/EApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjTravelPlatformV3.Models;
+
+namespace prjTravelPlatformV3.Areas.Employee.Controllers.Employee
+{
+    [Route("/api/Employee/{action}/{id?}")]
+    public class EApiController : Controller
+    {
+        private readonly dbTravalPlatformContext _context;
+
+        public EApiController(dbTravalPlatformContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetAll()
+        {
+            var employees = from e in _context.TEmployees
+                         select e;
+            return Json(employees);
+        }
+        public IActionResult GetById(int id)
+        {
+            var employee = _context.TEmployees.FirstOrDefault(e => e.FEmployeeId == id);
+            return Json(employee);
+        }
+    }
+}
---
+++ prjTravelPlatformV3/Areas/Employee/Controllers/Employee/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjTravelPlatformV3.Models;
+
+namespace prjTravelPlatformV3.Areas.Employee.Controllers.Employee
+{
+    [Area("Employee")]
+    public class EmployeeController : Controller
+    {
+        private readonly dbTravalPlatformContext _context;
+        public EmployeeController(dbTravalPlatformContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
